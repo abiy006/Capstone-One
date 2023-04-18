29733efacefa423c48f1fac47458a7f0c4be5db8
Abiy Hussen Aman <AbiyHussen@gmail.com>
2023-04-18 22:42:18 +0300

    object is created

---
+++ index.js
@@ -0,0 +1,13 @@
+import data from './data.js';
+// const data = require('data.js');
+
+// const data = require('./employees.json');
+
+console.log(data);
+
+// const works = document.getElementById('card');
+
+// works.style.width = '100px';
+// works.style.height = '100px';
+// works.style.backgroundColor = 'red';
+// works.textContent = 'fgfgfgfgfg';
